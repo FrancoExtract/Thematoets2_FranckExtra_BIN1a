c720b50f1e59e2b5a9d793ce3cd07fb21816a513
FrancoExtract <90254170+FrancoExtract@users.noreply.github.com>
2022-01-23 21:44:47 +0100

    Update Thematoets 2 Franck Extra.py

--- Thematoets 2 Franck Extra.py
+++ Thematoets 2 Franck Extra.py
@@ -4,7 +4,8 @@
 try:
     import re
     import matplotlib.pyplot as plt
-    import tkinter as tk
+    import tkinter
+    from tkinter import messagebox
 except ModuleNotFoundError:
     print("Matplotlib module not found")  # It can't find the module for some reason...
 
@@ -23,9 +24,50 @@ def read_file():
 # plt.ylabel("Kinase count")
 # plt.show()
 
+class GUI:
+    def __init__(self):
+        # Main window
+        self.main_window = tkinter.Tk()
 
-if __name__ == '__main__':
+        # Top and bottom frames
+        self.top_frame = tkinter.Frame(self.main_window)
+        self.bottom_frame = tkinter.Frame(self.main_window)
+        self.top_frame.pack()
+        self.bottom_frame.pack()
+
+        # Labels
+        self.label1 = tkinter.Label(self.main_window, text="Hello top frame!")
+        self.label1.pack()
+        self.label2 = tkinter.Label(self.bottom_frame,
+                                    text="Hello bottom frame!")
+        self.label2.pack()
+
+        # Buttons
+        self.buttons = tkinter.Button(self.bottom_frame,  # Where the button is
+                                      text="Click here lol",  # What text is shown
+                                      command=self.do_something)  # What should happen when the button is pressed
+        self.buttons.pack()
+        self.quit_button = tkinter.Button(self.bottom_frame,
+                                          text="Quit popup",
+                                          command=self.main_window.destroy)
+        self.quit_button.pack()
+
+        # Shows main window
+        tkinter.mainloop()
+
+    def do_something(self):
+        tkinter.messagebox.showinfo("Response",
+                                    "Haha ur mom")
+
+
+def main():
     gff = "GCF_000013425.1_ASM1342v1_genomic.gff"
 
-# Prototype graph
-# graph(([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), ([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]))
+    # GUI setup
+    gui = GUI()
+
+    # Prototype graph
+    # graph(([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), ([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]))
+
+
+main()
